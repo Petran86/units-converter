3e22512e648051fcba596f87026acb7b3a40c648
Petros Kouzoupis <peterkuzu@gmail.com>
2025-01-25 15:12:18 +0200

    Completed project
    added checks for number inputs and dark theme switch

--- index.js
+++ index.js
@@ -9,14 +9,18 @@ const convertBtn = document.getElementById("convert-btn")
 const resultsLength = document.getElementById("results-length")
 const resultsVolume = document.getElementById("results-volume")
 const resultsMass = document.getElementById("results-mass")
+const themeSwitch = document.getElementById("theme-switch")
 
 convertBtn.addEventListener("click", unitConversion)
 
+themeSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("darkmode")
+})
+
 function unitConversion() {
     const value = inputNumber.value
-    const regex = /^[0-9]+$/
 
-    if (value.match(regex)) {
+    if (isPositiveNumber(value)) {
         resultsLength.innerHTML = `<p>
             ${value} meters = ${(value * 3.281).toFixed(3)} feet | 
             ${value} feet = ${((1 / 3.281) * value).toFixed(3)} meters
@@ -35,4 +39,9 @@ function unitConversion() {
         alert("Must input number")
     }
     
+}
+
+function isPositiveNumber(value) {
+    const number = parseFloat(value)
+    return !isNaN(number) && number > 0
 }
